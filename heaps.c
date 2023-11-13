678468c4e5a31e5254db4013aed7e2a398bc085a
unknown <borja.klenn@gmail.com>
2023-11-13 18:39:18 +0800

    third

--- heaps.c
+++ heaps.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 #include "heaps.h"
 
+MINHEAP *initializeHeap(int capacity){
+    MINHEAP *temp = (MINHEAP*)malloc(sizeof(MINHEAP));
+    temp->capacity = capacity;
+    temp->size = 0;
+    temp->array = (int*)malloc(capacity * sizeof(int)); //Allocate memory for the pointer array.
+    return temp;
+}
+
+void insert(MINHEAP *heap, int num){
+    if(heap->size == 0){
+        heap->array[heap->size] = num;
+        heap->size++;
+        return;
+    }
+    //...
+}
+
 int main(){
     return 0;
 }
--- heaps.h
+++ heaps.h
@@ -5,14 +5,39 @@
 
 
 
-typedef struct heap{
-    int *array;
-    int capacity;
-    int size;
-} HEAP;
+#include <stdlib.h>
 
-void insert(HEAP *heap, int num);
-void deleteNum(HEAP *heap);
-HEAP *buildHeap(int *array, int size);
+// Structure representing a min-heap
+typedef struct MINHEAP{
+    int* array;    // Array to store heap elements
+    int capacity;  // Maximum capacity of the heap
+    int size;      // Current size of the heap
+} MINHEAP;
 
+// Function to initialize a min-heap
+MINHEAP *initializeHeap(int capacity);
+
+// Function to insert a new element into the min-heap
+void insert(MINHEAP* heap, int value);
+
+// Function to delete the minimum element from the min-heap
+int deleteMin(MINHEAP* heap);
+
+// Function to heapify a subtree rooted with the given index
+void heapify(MINHEAP* heap, int index);
+
+// Function to build a min-heap from an array of elements
+MINHEAP *buildHeap(int* array, int size);
+
+// Function to get the minimum element from the min-heap without deleting it
+int getMin(MINHEAP* heap);
+
+// Function to check if the min-heap is empty
+int isEmpty(MINHEAP* heap);
+
+// Function to check if the min-heap is full
+int isFull(MINHEAP* heap);
+
+// Function to free the memory allocated for the min-heap
+void destroyHeap(MINHEAP* heap);
 #endif
